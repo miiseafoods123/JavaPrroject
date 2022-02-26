721e0c527e1b9a8dde88d2d68a3151319dce6998
miiseafoods123 <miiseafoods1@gmail.com>
2022-02-27 00:22:22 +0530

    task done

---
+++ src/org/hexa/Employee.java
@@ -0,0 +1,13 @@
+package org.hexa;
+
+public class Employee {
+	public void empDetails() {
+		System.out.println("srinath");	
+
+	}
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.empDetails();
+	}
+
+}
